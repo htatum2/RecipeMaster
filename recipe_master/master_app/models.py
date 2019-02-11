185af21d63b7053dd40b2efdc6d94a6f0aa415f5
jav_solo <javiersolorzano@icloud.com>
2019-02-10 22:57:10 -0600

    Add User Profile Functionality

--- recipe_master/master_app/models.py
+++ recipe_master/master_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# You must save the file and run python manage.py makemigrations 
+# for changes to take effect
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    recipe_creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.recipe_name 
--- recipe_master/master_app/views.py
+++ recipe_master/master_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'master_app/home.html')
+    context = {
+        'recipes': Recipe.objects.all()
+    }
+    return render(request, 'master_app/home.html', context)
 
 def about(request):
     return render(request, 'master_app/about.html')
