0d8e6508877e19847ef12f2f075e00fefa0148fc
Alyssa N. Smith <46791420+AlyssaNoelSmith@users.noreply.github.com>
2019-02-16 17:32:35 -0600

    Update models.py
    Add fields to Recipe Class, create class to facilitate limitations on float values for ratings

--- recipe_master/master_app/models.py
+++ recipe_master/master_app/models.py
@@ -1,20 +1,48 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import numpy as np
 
 # You must save the file and run python manage.py makemigrations 
 # for changes to take effect
+class MinMaxFloat(models.FloatField):
+    def __init__(self, min_value=None, max_value=None, *args, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        super(MinMaxFloat, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value' : self.max_value}
+        defaults.update(kwargs)
+        return super(MinMaxFloat, self).formfield(**defaults)
+
+
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length=100)
-    #Won't let me set a max/min value??
-    rating=models.IntegerField(1)
+   # rating=models.FloatRangeField(min_value=1.0, max_value=5.0)
+    overallRating = MinMaxFloat(min_value=0.0, max_value=5.0)
+    authenticityRating = MinMaxFloat(min_value=0.0, max_value=5.0)
+    mealPrepTimeMinutes=models.IntegerField(1)
     #diettype = models.CharField(max_length=10)
     #tags=models.ManyToManyField(String)
+    descriptionTags = np.array([])
     image = models.ImageField(default='food_default.jpg', upload_to='recipe_pics')
     content = models.TextField(max_length=100)
     date_posted = models.DateTimeField(default=timezone.now)
     recipe_creator = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.recipe_name,"Rating = " + self.rating
+        return self.recipe_name,"Rating = " + self.overallRating
+
+    #def display(self):
+
+"""
+    class FloatRangeField(models.FloatField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.FloatField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(FloatRangeField, self).formfield(**defaults)
+"""
