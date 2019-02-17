2321f73e9ade0c4efeba339a80d5f5baec2c2183
Casey Campbell <ccsoup9727@gmail.com>
2019-02-16 18:01:31 -0600

    Removed search index settings (errors).

--- recipe_master/recipe_master/settings.py
+++ recipe_master/recipe_master/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
+
+  
+
 
 ]
 
@@ -132,14 +134,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'master-home'
 LOGIN_URL = 'login'
 
-# Web Search Function
-# http://haystacksearch.org
-# Currently working with establishing indexes and the like....
 
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
-        'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'haystack',
-    },
-}
