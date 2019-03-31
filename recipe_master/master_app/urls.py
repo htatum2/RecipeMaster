c39fe9daf6bf57d50e1fed84413c7087fa08b647
jav_solo <javiersolorzano@icloud.com>
2019-03-30 19:21:11 -0500

    update db

--- recipe_master/master_app/urls.py
+++ recipe_master/master_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.conf.urls import url, include
+#from django.conf.urls import url, include
 from .views import RecipeListView, RecipeDetailView
 from . import views
 
