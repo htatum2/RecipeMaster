7750269571c137c21ec230fa4b0e75909dfc006d
Alyssa N. Smith <46791420+AlyssaNoelSmith@users.noreply.github.com>
2019-03-31 17:49:22 -0500

    Update urls and search.html

--- recipe_master/master_app/urls.py
+++ recipe_master/master_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('recipe/<pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('about/', views.about, name='master-about'),
     path('search/', views.search, name='master-search'),
+    path('search/', views.RecipeSearchListView, name='RecipeSearch'),
+    
     path('profile/', views.profile, name='master-profile'),
     path('search/', views.search,name='searchengine-search'),
     path('recipes/', views.recipes,name='master-recipes'),
