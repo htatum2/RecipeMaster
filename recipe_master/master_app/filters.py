c81f9af66b9c7d841c9e6234f537245ea83a3952
htatum2 <haleytatum2@gmail.com>
2019-04-01 21:34:21 -0500

    Adding more Filters/sorting

--- recipe_master/master_app/filters.py
+++ recipe_master/master_app/filters.py
@@ -1,14 +1,16 @@
-from .models import Recipe
+from .models import Recipe, Review, RecipeReview
+from django import forms
 import django_filters
 
 class RecipeFilter(django_filters.FilterSet):
-    #country = django_filters.CharFilter(lookup_expr='icontains')
+    country = django_filters.CharFilter(lookup_expr='icontains')
+    #Rating = django_filters.ModelMultipleChoiceFilter(queryset = RecipeReview.objects.all(), widget = forms.CheckboxSelectMultiple)
     #meal_PrepTime_Minutes_lt = django_filters.NumberFilter(name = 'meal_PrepTime_Minutes', lookup_expr='lt')
     #calories_lt= django_filters.NumberFilter(name='calories', lookup_expr='lt')
     #calories_gt= django_filters.NumberFilter(name='calories', lookup_expr='gt')
     class Meta:
         model=Recipe
-        fields = ['country', 'calories','meal_PrepTime_Minutes']
+        fields = ['country', 'calories','meal_PrepTime_Minutes','category']
 
 
 
