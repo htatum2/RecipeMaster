c4b438aed6ad33cd69f4bee1fcef0c7544093c18
htatum2 <haleytatum2@gmail.com>
2019-04-02 21:01:48 -0500

    Removing django-star-filter

--- recipe_master/recipe_master/urls.py
+++ recipe_master/recipe_master/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
     path('', include('master_app.urls')),
 ]
 
