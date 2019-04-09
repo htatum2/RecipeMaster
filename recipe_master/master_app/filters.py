7326b913d64fd2670d8599ce1f8a1cdfc82ff538
Haley Tatum <haleytatum2@gmail.com>
2019-04-09 05:44:05 -0500

    Add details to filters

--- recipe_master/master_app/filters.py
+++ recipe_master/master_app/filters.py
@@ -5,15 +5,14 @@ import django_filters
 class RecipeFilter(django_filters.FilterSet):
     country = django_filters.CharFilter(lookup_expr='icontains')
     #Rating = django_filters.ModelMultipleChoiceFilter(queryset = RecipeReview.objects.all(), widget = forms.CheckboxSelectMultiple)
-    meal_PrepTime_Minutes = django_filters.NumberFilter(lookup_expr='lt')
-    calories = django_filters.NumberFilter( lookup_expr='lt')
-    calories = django_filters.NumberFilter(lookup_expr='gt')
-    average_rating = django_filters.NumberFilter(lookup_expr='gt')
-    authenticity= django_filters.NumberFilter(lookup_expr='gt')
-
+    meal_PrepTime_Minutes = django_filters.NumberFilter(lookup_expr='lt', label='Total Meal Prep/Cook Time')
+    calories = django_filters.NumberFilter( lookup_expr='lt', label='Calories Less Than')
+    calories_lt = django_filters.NumberFilter(lookup_expr='gt', label='Calories Greater Than')
+    average_rating = django_filters.NumberFilter(lookup_expr='gt', label = 'Overall Rating')
+    authenticity= django_filters.NumberFilter(lookup_expr='gt',label='Authenticity Rating')
     class Meta:
         model=Recipe
-        fields = ['country', 'calories','meal_PrepTime_Minutes','category', 'average_rating', 'authenticity']
+        fields = ['country', 'calories','calories_lt','meal_PrepTime_Minutes','category', 'average_rating', 'authenticity']
 
 
 
